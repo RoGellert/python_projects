f7a5565eb4908e89d19d0b81ee4032d546e53e04
Roman <titangtace@gmail.com>
2023-11-15 19:53:15 +0100

    Further progress

--- morse_converter/morse_converter.py
+++ morse_converter/morse_converter.py
@@ -1,4 +1,34 @@
 from morse_dicts import letterToMorseDict, morseToLetterDict
+import sys
+
+
+def convert(argv: list[str]):
+    if not len(argv) == 4:
+        raise Exception("Invalid argument num")
+
+    if argv[1] not in ["from_morse", "to_morse"]:
+        raise Exception("Invalid operation type argument\n " +
+                        "Accepted arguments: [from_morse, to_morse]")
+
+    return
+
+
+def preprocess_string(text: list[str]) -> list[str]:
+    if text[len(text)-1] == "":
+        for i in range(len(text)-1):
+            text[i] = text[i][:-1]
+    else:
+        for i in range(len(text)):
+            text[i] = text[i][:-1]
+
+    return text
+
 
 if __name__ == "__main__":
-    print(letterToMorseDict["A"])
+    with open("test_input1.txt") as f:
+        lines = f.readlines()
+    #convert(sys.argv)
+    print(lines)
+    print(preprocess_string(lines))
+    f.close()
+

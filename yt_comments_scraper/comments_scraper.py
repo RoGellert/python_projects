4d3e3936dd18aaf17a65229208aefc9e66ad9d7b
Roman Gellert <titangtace@gmail.com>
2023-12-06 19:56:18 +0100

    Added possibility to pause video

--- yt_comments_scraper/comments_scraper.py
+++ yt_comments_scraper/comments_scraper.py
@@ -1,5 +1,6 @@
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 import bs4 as soup
 import time
@@ -18,14 +19,28 @@ if __name__ == '__main__':
 
     driver.get(url)
     driver.maximize_window()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(13)
 
     #confirmation = driver.find_element(By.XPATH, '//div[@class="eom-button-row style-scope '
                       #                           'ytd-consent-bump-v2-lightbox"]')
     #confirmation.click()
 
-    confirmation = driver.find_element(By.XPATH, '(//ytd-button-renderer[@class="style-scope ytd-consent-bump-v2-lightbox"])[3]')
-    confirmation.click()
+    actions = ActionChains(driver)
+
+    element = driver.find_element(By.XPATH, '(//ytd-button-renderer[@class="style-scope ytd-consent-bump-v2-lightbox"])[3]')
+    actions.click(element).perform()
+
+    driver.refresh()
+
+    element = driver.find_element(By.XPATH,'//video')
+    actions.move_to_element(to_element=element).perform()
+
+    element = driver.find_element(By.XPATH,'//button[@aria-keyshortcuts="k"]')
+    actions.click(element).perform()
+
+    actions.scroll_by_amount(0, 1000).perform()
+    actions.scroll_by_amount(0, 100000).perform()
+
 
 
 

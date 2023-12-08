a6b5d89909261e952633c8136a4706d8d09e9776
Roman Gellert <titangtace@gmail.com>
2023-12-08 17:24:28 +0100

    Scraping

--- yt_comments_scraper/comments_scraper.py
+++ yt_comments_scraper/comments_scraper.py
@@ -6,19 +6,15 @@ from bs4 import BeautifulSoup
 import time
 
 
-url = 'https://www.youtube.com/watch?v=O5BJVO3PDeQ&t=2414s'
-chromedriver_folder = '/chromedriver.exe'
-
-
 if __name__ == '__main__':
     url = 'https://www.youtube.com/watch?v=O5BJVO3PDeQ&t=2414s'
 
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
     actions = ActionChains(driver)
 
     driver.get(url)
+    driver.maximize_window()
     driver.implicitly_wait(13)
 
     element = driver.find_element(By.XPATH, '(//ytd-button-renderer[@class="style-scope ytd-consent-bump-v2-lightbox"])[3]')
@@ -33,9 +29,13 @@ if __name__ == '__main__':
     actions.click(element).perform()
 
     actions.scroll_by_amount(0, 1000).perform()
+    driver.implicitly_wait(6)
+    actions.scroll_by_amount(0, 1000).perform()
+    driver.implicitly_wait(6)
 
-    comments = driver.find_element(By.XPATH, '//*[@id="comments"]')
+    comments = driver.find_element(By.XPATH, '//yt-formatted-string[@id="content-text" and @slot="content"]')
     comments_data = comments.get_attribute('innerHTML')
 
     comments_data_soup = BeautifulSoup(comments_data, 'html.parser')
     print(comments_data_soup)
+    #comments_data_soup.find("yt-formatted-string", {"id": "content-text"})

1bfced450a6a999e8211fe71990869494f994e80
Roman Gellert <titangtace@gmail.com>
2023-12-14 11:21:50 +0100

    WIP

--- yt_comments_scraper/comments_scraper.py
+++ yt_comments_scraper/comments_scraper.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
-
 if __name__ == '__main__':
     url = 'https://www.youtube.com/watch?v=O5BJVO3PDeQ&t=2414s'
 
@@ -14,7 +13,8 @@ if __name__ == '__main__':
     driver.maximize_window()
     driver.implicitly_wait(10)
 
-    element = driver.find_element(By.XPATH, '(//ytd-button-renderer[@class="style-scope ytd-consent-bump-v2-lightbox"])[3]')
+    element = driver.find_element(By.XPATH, '(//ytd-button-renderer[@class="style-scope '
+                                            'ytd-consent-bump-v2-lightbox"])[3]')
     actions.click(element).perform()
 
     driver.refresh()
@@ -27,18 +27,22 @@ if __name__ == '__main__':
     driver.implicitly_wait(10)
 
     actions.scroll_by_amount(0, 1000).perform()
-    for i in range(50):
-        actions.scroll_by_amount(0, 10000).perform()
+    for i in range(100):
+        actions.scroll_by_amount(0, 30000).perform()
         driver.implicitly_wait(6)
-    driver.implicitly_wait(6)
+    driver.implicitly_wait(15)
 
-
-    comments = driver.find_elements(By.XPATH, '//ytd-comment-renderer[@id="comment"]')
+    comments = driver.find_elements(By.XPATH, '//div[@id="comment-content"]')
     comments_list = []
     for comment in comments:
         comment_to_append = []
-        for line in BeautifulSoup(comment.get_attribute('innerHTML'), 'html.parser').find_all("span", {"class": "style-scope yt-formatted-string"}):
-            comment_to_append.append(line.text)
-        comments_list.append(comment_to_append)
+        content = BeautifulSoup(comment.get_attribute('innerHTML'), 'html.parser')
+        lines = content.find_all("span", {"class": "style-scope yt-formatted-string"})
+        for line in lines:
+            if line.text != '\n':
+                comment_to_append.append(line.text)
+        if comment_to_append:
+            comments_list.append(comment_to_append)
 
-    print(comments_list)
+    for i in comments_list:
+        print(i)

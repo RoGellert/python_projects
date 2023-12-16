9090b73489fc1f48982484e1c92806e8baf83f93
Roman Gellert <titangtace@gmail.com>
2023-12-16 18:11:08 +0100

    WIP

--- yt_comments_scraper/comments_scraper.py
+++ yt_comments_scraper/comments_scraper.py
@@ -6,7 +6,9 @@ from bs4 import BeautifulSoup
 if __name__ == '__main__':
     url = 'https://www.youtube.com/watch?v=O5BJVO3PDeQ&t=2414s'
 
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--lang=en-US")
+    driver = webdriver.Chrome(options=options)
     actions = ActionChains(driver)
 
     driver.get(url)
@@ -27,22 +29,33 @@ if __name__ == '__main__':
     driver.implicitly_wait(10)
 
     actions.scroll_by_amount(0, 1000).perform()
-    for i in range(200):
+    driver.implicitly_wait(5)
+    for i in range(100):
         actions.scroll_by_amount(0, 2000).perform()
-        driver.implicitly_wait(1)
+        driver.implicitly_wait(5)
     driver.implicitly_wait(15)
 
-    comments = driver.find_elements(By.XPATH, '//div[@id="comment-content"]')
+    comments = driver.find_elements(By.XPATH, '//ytd-comment-renderer[@id="comment"]')
     comments_list = []
     for comment in comments:
         comment_to_append = []
+
         content = BeautifulSoup(comment.get_attribute('innerHTML'), 'html.parser')
-        lines = content.find_all("span", {"class": "style-scope yt-formatted-string"})
+        lines = content.find_all("yt-formatted-string", {"id": "content-text"})
+
+        likes = content.find("span", {"id": "vote-count-middle"})
+        likes = likes.text.replace(" ", "").replace("\n", "")
+        likes = int(likes) if likes[-1] != 'K' else int(float(likes[0:-1]))*1000
+
         for line in lines:
-            if line.text != '\n':
-                comment_to_append.append(line.text)
-        if comment_to_append:
-            comments_list.append(comment_to_append)
+            line_text = line.text.replace("\n", " ")
+            line_text.replace("    ", " ")
+            line_text.replace("   ", " ")
+            line_text.replace("  ", " ")
+            comment_to_append.append(line_text)
+        comments_list.append([comment_to_append, likes])
 
     for i in comments_list:
         print(i)
+
+    print(type(comments_list[0][0]))

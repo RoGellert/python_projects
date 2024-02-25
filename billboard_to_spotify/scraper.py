d0ea2966a6333b30bf410f89a96fad403594d966
Roman Gellert <titangtace@gmail.com>
2024-02-25 13:23:53 +0100

    Initial commits

--- billboard_to_spotify/scraper.py
+++ billboard_to_spotify/scraper.py
@@ -6,7 +6,7 @@ import pandas as pd
 from datetime import datetime
 
 if __name__ == '__main__':
-    print("Write the date you want the top from in format YYYY-MM-DD:")
+    print("Write the date you want in format YYYY-MM-DD:")
 
     user_input = ""
     while True:
